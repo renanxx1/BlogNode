1421bb92cfb8cea735ded5f3788c14c1d9c81f82
Renan <renan980@gmail.com>
2020-10-28 10:54:18 -0300

    28/10

--- categories/CategoriesController.js
+++ categories/CategoriesController.js
@@ -5,4 +5,9 @@ router.get("/categories", (req, res) => {
     res.send("<h1>Rota de categoria</h1>");
 });
 
+router.get('/admin/categories/new', (req, res) => {
+    res.render('admin/categories/new');
+});
+
+
 module.exports = router;
