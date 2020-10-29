d8ff6ca5cfbfc2f952d4263e5a41e4554ae37eff
Renanxx1 <renan980@gmail.com>
2020-10-29 18:20:30 -0300

    29/10

--- categories/CategoriesController.js
+++ categories/CategoriesController.js
@@ -13,28 +13,62 @@ router.get('/admin/categories/new', (req, res) => {
 
 router.post('/categories/save', (req, res) => {
     var title = req.body.title;
-    if (title != undefined){
-        
+    if (title != undefined) {
+
         Category.create({
             title: title,
             slug: slugify(title)
-        }).then(()=>{
-            res.redirect('/');
-        })
+        }).then(() => {
+            res.redirect('/admin/categories/');
+        });
 
-    }else{
-          res.redirect('/admin/categories/new');
+    } else {
+        res.redirect('/admin/categories/new');
     }
-})
+});
 
 router.get("/admin/categories", (req, res) => {
     Category.findAll().then(categories => {
         res.render('admin/categories/index', {
-            categories:categories
+            categories: categories
         });
-    })
-    
-})
+    });
+});
 
+router.post("/admin/categories/delete", (req, res) => {
+    var id = req.body.id;
+    if (id != undefined) {
+        if (!isNaN(id)) {
+            Category.destroy({
+                where: {
+                    id: id
+                }
+            }).then(() => {
+                res.redirect("/admin/categories");
+            })
+        } else {
+            res.redirect("/admin/categories")
+        }
+    } else {
+        res.redirect("/admin/categories")
+    }
+});
+
+router.get("/admin/categories/edit/:id", (req, res) => {
+    var id = req.params.id;
+
+    if (isNaN(id)) {
+        res.redirect("/admin/categories");
+    }
+    Category.findByPk(id).then(category => {
+        if (category != undefined) {
+            res.render("admin/categories/edit", { category: category })
+        } else {
+            res.redirect("/admin/categories")
+        }
+    }).catch(erro => {
+        res.redirect("/admin/categories");
+    })
+})
 
 module.exports = router;
